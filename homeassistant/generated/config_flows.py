4959616a53e7fd937d6b347a0a82c1966ca9f73c
Assaf Akrabi <akrabi@users.noreply.github.com>
2026-04-17 15:32:20 +0300

    Update generated files for russound_rnet config flow

--- homeassistant/generated/config_flows.py
+++ homeassistant/generated/config_flows.py
@@ -620,6 +620,7 @@ FLOWS = {
         "rova",
         "rpi_power",
         "ruckus_unleashed",
+        "russound_rnet",
         "russound_rio",
         "ruuvi_gateway",
         "ruuvitag_ble",
