df796e0b6f3ad51a2a293c8590af45d6a4ec8412
Assaf Akrabi <akrabi@users.noreply.github.com>
2026-04-17 18:41:11 +0300

    Fix: await async_request_refresh (is a coroutine, not a callback)

--- homeassistant/components/russound_rnet/media_player.py
+++ homeassistant/components/russound_rnet/media_player.py
@@ -173,7 +173,7 @@ class RussoundRNETDevice(
             self._zone_within,
             round(volume * 50),
         )
-        self.coordinator.async_request_refresh()
+        await self.coordinator.async_request_refresh()
 
     async def async_turn_on(self) -> None:
         """Turn the media player on."""
@@ -183,7 +183,7 @@ class RussoundRNETDevice(
             self._zone_within,
             True,
         )
-        self.coordinator.async_request_refresh()
+        await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self) -> None:
         """Turn off media player."""
@@ -193,7 +193,7 @@ class RussoundRNETDevice(
             self._zone_within,
             False,
         )
-        self.coordinator.async_request_refresh()
+        await self.coordinator.async_request_refresh()
 
     async def async_mute_volume(self, mute: bool) -> None:
         """Send mute command.
@@ -205,7 +205,7 @@ class RussoundRNETDevice(
             self._controller_id,
             self._zone_within,
         )
-        self.coordinator.async_request_refresh()
+        await self.coordinator.async_request_refresh()
 
     async def async_select_source(self, source: str) -> None:
         """Set the input source."""
@@ -217,4 +217,4 @@ class RussoundRNETDevice(
                 self._zone_within,
                 source_id,
             )
-            self.coordinator.async_request_refresh()
+            await self.coordinator.async_request_refresh()
